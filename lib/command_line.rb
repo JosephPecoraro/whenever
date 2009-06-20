72c35225483f99ae64c9c14a5f04bed1a3005441
Joseph Pecoraro <joepeck02@gmail.com>
2009-06-20 16:12:13 -0400

    Minor fix, updated changelog and gem version

--- lib/command_line.rb
+++ lib/command_line.rb
@@ -40,7 +40,7 @@ module Whenever
     end
   
     def whenever_cron
-      @whenever_cron ||= [comment_open, "\n\n", Whenever.cron(@options), comment_open].join
+      @whenever_cron ||= [comment_open, "\n\n", Whenever.cron(@options), comment_close].join
     end
     
     def read_crontab
